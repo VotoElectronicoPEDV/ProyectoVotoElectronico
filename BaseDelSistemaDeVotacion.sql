aadced4c8589d0abfb0cad13d10c9d4ea5053a81
HectorOC-e <65907557+HectorOC-e@users.noreply.github.com>
2020-07-14 18:38:02 -0600

    Update BaseDelSistemaDeVotacion.sql

--- BaseDelSistemaDeVotacion.sql
+++ BaseDelSistemaDeVotacion.sql
@@ -10,6 +10,7 @@ NombrePartido varchar(50) not null,
 Lema varchar(100)
 )
 
+
 create table Presidente(
 idPresidente int identity primary key not null,
 PrimerNombre varchar(25) not null,
@@ -18,24 +19,58 @@ PrimerApellido varchar(25) not null,
 SegundoApellido varchar(25),
 VotosValidos int not null,
 VotosNulos int,
+EstadoVotacion varchar(25) not null,
 Partido int FOREIGN KEY REFERENCES PartidoPolitico(idPartido)
 )
---A donde ira a Votar o desde donde esta votando
-create table CentroDeVotacion(
-idCentroDeVotacion int not null primary key,
-Nombre varchar(45),
-Departamento varchar(30),
-Municipio varchar(30),
-Dirrecion varchar(50)
-)
---almacenara todos los departamentos
+
+
 create table Departamento(
-idDepartamento int not null primary key,
+idDepartamento varchar(2) not null primary key,
 nombreDepartamento varchar(50)
 )
---almacenara todos los municipios
+
+
 create table Municipio(
-idMunicipio int not null primary key,
+idMunicipio varchar(2) not null primary key,
 nombreMunicipio varchar(50),
-idDepartamento varchar(2)
+idDepartamento varchar(2) FOREIGN KEY REFERENCES Departamento(idDepartamento)
+)
+
+
+create table Alcalde(
+idAlcalde int identity primary key not null,
+PrimerNombre varchar(25) not null,
+SegundoNombre varchar(25),
+PrimerApellido varchar(25) not null,
+SegundoApellido varchar(25),
+VotosValidos int not null,
+VotosNulos int,
+EstadoVotacion varchar(25) not null,
+Partido int FOREIGN KEY REFERENCES PartidoPolitico(idPartido),
+Municipio varchar(2) FOREIGN KEY REFERENCES Municipio(idMunicipio)
+)
+
+create table Diputado(
+idAlcalde int identity primary key not null,
+PrimerNombre varchar(25) not null,
+SegundoNombre varchar(25),
+PrimerApellido varchar(25) not null,
+SegundoApellido varchar(25),
+VotosValidos int not null,
+VotosNulos int,
+EstadoVotacion varchar(25) not null,
+Partido int FOREIGN KEY REFERENCES PartidoPolitico(idPartido),
+Departamento varchar(2) FOREIGN KEY REFERENCES Departamento(idDepartamento)
+)
+
+create table votante(
+IdentidadVotante varchar(13) primary key not null,
+PrimerNombre varchar(25) not null,
+SegundoNombre varchar(25) not null,
+PrimerApellido varchar(25) not null,
+SegundoApellido varchar(25) not null,
+edad int not null,
+sexo varchar(10) not null,
+estadoVotante varchar(25) not null,
+CodMunicipio varchar(2) FOREIGN KEY REFERENCES Municipio(idMunicipio)
 )
