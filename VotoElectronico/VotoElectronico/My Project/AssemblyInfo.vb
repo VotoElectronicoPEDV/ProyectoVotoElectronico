f051b8d1abe03249defff81f0335c64b5c197996
Hector Castellanos <osoriohector89@gmail.com>
2020-07-19 21:28:20 -0600

    Conexion

---
+++ VotoElectronico/VotoElectronico/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("VotoElectronico")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VotoElectronico")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("9b48cdf5-6b4a-4cef-a9e8-1dd2ba0858e1")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ VotoElectronico/VotoElectronico/conexion.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+Public Class conexion
+    Public conexion As SqlConnection = New SqlConnection("Data Source=HECTOROS\SQLEXPRESS02;Initial Catalog=SistemaNacionalDeElecciones;Integrated Security=True")
+    Public lectura As SqlDataReader
+    Public comando As SqlCommand
+
+
+    Public Sub conectar()
+        Try
+            conexion.Open()
+            MessageBox.Show("Conectado con exito")
+        Catch ex As Exception
+            MessageBox.Show("No se ha podido acceder a la base de datos")
+        Finally
+            conexion.Close()
+        End Try
+    End Sub
+
+End Class
