0add780f2476cf2a334b5e261ee20dcfcf212617
Denia Chavarria <julissachavarria884@gmail.com>
2020-07-20 19:44:26 -0500

    form votante

--- VotoElectronico/VotoElectronico/My Project/Application.Designer.vb
+++ VotoElectronico/VotoElectronico/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VotoElectronico.Form1
+            Me.MainForm = Global.VotoElectronico.VotacionInterna
         End Sub
     End Class
 End Namespace
---
+++ VotoElectronico/VotoElectronico/VotacionInterna.vb
@@ -0,0 +1,9 @@
+﻿Public Class VotacionInterna
+    Private Sub txtidentidad_TextChanged(sender As Object, e As EventArgs) Handles txtidentidad.TextChanged
+
+    End Sub
+
+    Private Sub VotacionInterna_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
