9cb0fcebb0b3ca9be8f377776a2cfc2b40fa7ceb
gissellopez884@gmail.com <gissellopez884@gmail.com>
2020-07-23 23:01:04 -0500

    validaciones

--- VotoElectronico/VotoElectronico/Crud_Alcaldes.Designer.vb
+++ VotoElectronico/VotoElectronico/Crud_Alcaldes.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Crud_Alcaldes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.dgvAlcaldes = New System.Windows.Forms.DataGridView()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
@@ -29,6 +30,8 @@ Partial Class Crud_Alcaldes
         Me.Label8 = New System.Windows.Forms.Label()
         Me.cmbOperacion = New System.Windows.Forms.ComboBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmbMunicipio = New System.Windows.Forms.ComboBox()
+        Me.Label9 = New System.Windows.Forms.Label()
         Me.cmbEstado = New System.Windows.Forms.ComboBox()
         Me.cmbPartido = New System.Windows.Forms.ComboBox()
         Me.txtPrimerApellido = New System.Windows.Forms.TextBox()
@@ -43,20 +46,23 @@ Partial Class Crud_Alcaldes
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.cmbMunicipio = New System.Windows.Forms.ComboBox()
-        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ErrorValidacion = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
         Me.GroupBox3.SuspendLayout()
         CType(Me.dgvAlcaldes, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.ErrorValidacion, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.dgvAlcaldes)
-        Me.GroupBox3.Location = New System.Drawing.Point(12, 263)
+        Me.GroupBox3.Location = New System.Drawing.Point(9, 214)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(776, 175)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox3.Size = New System.Drawing.Size(582, 142)
         Me.GroupBox3.TabIndex = 19
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Visualización de datos"
@@ -67,12 +73,13 @@ Partial Class Crud_Alcaldes
         Me.dgvAlcaldes.AllowUserToDeleteRows = False
         Me.dgvAlcaldes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgvAlcaldes.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.dgvAlcaldes.Location = New System.Drawing.Point(3, 18)
+        Me.dgvAlcaldes.Location = New System.Drawing.Point(2, 15)
+        Me.dgvAlcaldes.Margin = New System.Windows.Forms.Padding(2)
         Me.dgvAlcaldes.Name = "dgvAlcaldes"
         Me.dgvAlcaldes.ReadOnly = True
         Me.dgvAlcaldes.RowHeadersWidth = 51
         Me.dgvAlcaldes.RowTemplate.Height = 24
-        Me.dgvAlcaldes.Size = New System.Drawing.Size(770, 154)
+        Me.dgvAlcaldes.Size = New System.Drawing.Size(578, 125)
         Me.dgvAlcaldes.TabIndex = 0
         '
         'GroupBox2
@@ -80,18 +87,21 @@ Partial Class Crud_Alcaldes
         Me.GroupBox2.Controls.Add(Me.btnEjecutar)
         Me.GroupBox2.Controls.Add(Me.Label8)
         Me.GroupBox2.Controls.Add(Me.cmbOperacion)
-        Me.GroupBox2.Location = New System.Drawing.Point(621, 12)
+        Me.GroupBox2.Location = New System.Drawing.Point(466, 10)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(167, 245)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Size = New System.Drawing.Size(125, 199)
         Me.GroupBox2.TabIndex = 18
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Operaciones Crud"
         '
         'btnEjecutar
         '
-        Me.btnEjecutar.Location = New System.Drawing.Point(29, 153)
+        Me.btnEjecutar.Location = New System.Drawing.Point(22, 124)
+        Me.btnEjecutar.Margin = New System.Windows.Forms.Padding(2)
         Me.btnEjecutar.Name = "btnEjecutar"
-        Me.btnEjecutar.Size = New System.Drawing.Size(121, 25)
+        Me.btnEjecutar.Size = New System.Drawing.Size(91, 20)
         Me.btnEjecutar.TabIndex = 2
         Me.btnEjecutar.Text = "Ejecutar"
         Me.btnEjecutar.UseVisualStyleBackColor = True
@@ -99,9 +109,10 @@ Partial Class Crud_Alcaldes
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(26, 57)
+        Me.Label8.Location = New System.Drawing.Point(20, 46)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(74, 17)
+        Me.Label8.Size = New System.Drawing.Size(56, 13)
         Me.Label8.TabIndex = 1
         Me.Label8.Text = "Operación"
         '
@@ -109,9 +120,10 @@ Partial Class Crud_Alcaldes
         '
         Me.cmbOperacion.FormattingEnabled = True
         Me.cmbOperacion.Items.AddRange(New Object() {"Ingresar", "Actualizar", "Eliminar", "Consultar"})
-        Me.cmbOperacion.Location = New System.Drawing.Point(29, 82)
+        Me.cmbOperacion.Location = New System.Drawing.Point(22, 67)
+        Me.cmbOperacion.Margin = New System.Windows.Forms.Padding(2)
         Me.cmbOperacion.Name = "cmbOperacion"
-        Me.cmbOperacion.Size = New System.Drawing.Size(121, 24)
+        Me.cmbOperacion.Size = New System.Drawing.Size(92, 21)
         Me.cmbOperacion.TabIndex = 0
         '
         'GroupBox1
@@ -132,157 +144,180 @@ Partial Class Crud_Alcaldes
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 10)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(595, 245)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(446, 199)
         Me.GroupBox1.TabIndex = 17
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Datos Alcalde"
+        '
+        'cmbMunicipio
+        '
+        Me.cmbMunicipio.FormattingEnabled = True
+        Me.cmbMunicipio.Items.AddRange(New Object() {"Comayagua", "Ajuterique", "El Rosario", "Esquías", "Humuya", "La Libertad", "Lamaní", "La Trinidad", "Lejamaní", "Meámbar", "Minas de Oro", "Ojos de Agua", "San Jerónimo", "San José de Comayagua", "San José del Potrero", "San Luis", "San Sebastián", "Siguatepeque", "Villa de San Antonio", "Las Lajas", "Taulabé"})
+        Me.cmbMunicipio.Location = New System.Drawing.Point(341, 149)
+        Me.cmbMunicipio.Margin = New System.Windows.Forms.Padding(2)
+        Me.cmbMunicipio.Name = "cmbMunicipio"
+        Me.cmbMunicipio.Size = New System.Drawing.Size(92, 21)
+        Me.cmbMunicipio.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(247, 154)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(52, 13)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Municipio"
         '
         'cmbEstado
         '
         Me.cmbEstado.FormattingEnabled = True
         Me.cmbEstado.Items.AddRange(New Object() {"activo"})
-        Me.cmbEstado.Location = New System.Drawing.Point(454, 130)
+        Me.cmbEstado.Location = New System.Drawing.Point(340, 106)
+        Me.cmbEstado.Margin = New System.Windows.Forms.Padding(2)
         Me.cmbEstado.Name = "cmbEstado"
-        Me.cmbEstado.Size = New System.Drawing.Size(121, 24)
+        Me.cmbEstado.Size = New System.Drawing.Size(92, 21)
         Me.cmbEstado.TabIndex = 15
         '
         'cmbPartido
         '
         Me.cmbPartido.FormattingEnabled = True
         Me.cmbPartido.Items.AddRange(New Object() {"Partido Nacional", "Partido Libre", "Partido AntiCorrupcion", "Partido Liberal"})
-        Me.cmbPartido.Location = New System.Drawing.Point(455, 75)
+        Me.cmbPartido.Location = New System.Drawing.Point(341, 61)
+        Me.cmbPartido.Margin = New System.Windows.Forms.Padding(2)
         Me.cmbPartido.Name = "cmbPartido"
-        Me.cmbPartido.Size = New System.Drawing.Size(121, 24)
+        Me.cmbPartido.Size = New System.Drawing.Size(92, 21)
         Me.cmbPartido.TabIndex = 14
         '
         'txtPrimerApellido
         '
-        Me.txtPrimerApellido.Location = New System.Drawing.Point(160, 195)
+        Me.txtPrimerApellido.Location = New System.Drawing.Point(120, 158)
+        Me.txtPrimerApellido.Margin = New System.Windows.Forms.Padding(2)
         Me.txtPrimerApellido.Name = "txtPrimerApellido"
-        Me.txtPrimerApellido.Size = New System.Drawing.Size(120, 22)
+        Me.txtPrimerApellido.Size = New System.Drawing.Size(91, 20)
         Me.txtPrimerApellido.TabIndex = 4
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(328, 33)
+        Me.Label7.Location = New System.Drawing.Point(246, 27)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(119, 17)
+        Me.Label7.Size = New System.Drawing.Size(90, 13)
         Me.Label7.TabIndex = 13
         Me.Label7.Text = "Segundo Apellido"
         '
         'txtIdentidad
         '
-        Me.txtIdentidad.Location = New System.Drawing.Point(160, 30)
+        Me.txtIdentidad.Location = New System.Drawing.Point(120, 24)
+        Me.txtIdentidad.Margin = New System.Windows.Forms.Padding(2)
         Me.txtIdentidad.MaxLength = 13
         Me.txtIdentidad.Name = "txtIdentidad"
-        Me.txtIdentidad.Size = New System.Drawing.Size(120, 22)
+        Me.txtIdentidad.Size = New System.Drawing.Size(91, 20)
         Me.txtIdentidad.TabIndex = 0
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(328, 82)
+        Me.Label6.Location = New System.Drawing.Point(246, 67)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(53, 17)
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
         Me.Label6.TabIndex = 12
         Me.Label6.Text = "Partido"
         '
         'txtSegundoNombre
         '
-        Me.txtSegundoNombre.Location = New System.Drawing.Point(160, 132)
+        Me.txtSegundoNombre.Location = New System.Drawing.Point(120, 107)
+        Me.txtSegundoNombre.Margin = New System.Windows.Forms.Padding(2)
         Me.txtSegundoNombre.Name = "txtSegundoNombre"
-        Me.txtSegundoNombre.Size = New System.Drawing.Size(120, 22)
+        Me.txtSegundoNombre.Size = New System.Drawing.Size(91, 20)
         Me.txtSegundoNombre.TabIndex = 5
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(328, 137)
+        Me.Label5.Location = New System.Drawing.Point(246, 111)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(52, 17)
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
         Me.Label5.TabIndex = 11
         Me.Label5.Text = "Estado"
         '
         'txtPrimerNombre
         '
-        Me.txtPrimerNombre.Location = New System.Drawing.Point(160, 77)
+        Me.txtPrimerNombre.Location = New System.Drawing.Point(120, 63)
+        Me.txtPrimerNombre.Margin = New System.Windows.Forms.Padding(2)
         Me.txtPrimerNombre.Name = "txtPrimerNombre"
-        Me.txtPrimerNombre.Size = New System.Drawing.Size(120, 22)
+        Me.txtPrimerNombre.Size = New System.Drawing.Size(91, 20)
         Me.txtPrimerNombre.TabIndex = 6
         '
         'txtSegundoApellido
         '
-        Me.txtSegundoApellido.Location = New System.Drawing.Point(455, 30)
+        Me.txtSegundoApellido.Location = New System.Drawing.Point(341, 24)
+        Me.txtSegundoApellido.Margin = New System.Windows.Forms.Padding(2)
         Me.txtSegundoApellido.Name = "txtSegundoApellido"
-        Me.txtSegundoApellido.Size = New System.Drawing.Size(120, 22)
+        Me.txtSegundoApellido.Size = New System.Drawing.Size(91, 20)
         Me.txtSegundoApellido.TabIndex = 3
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(27, 200)
+        Me.Label4.Location = New System.Drawing.Point(20, 162)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(103, 17)
+        Me.Label4.Size = New System.Drawing.Size(76, 13)
         Me.Label4.TabIndex = 10
         Me.Label4.Text = "Primer Apellido"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(27, 33)
+        Me.Label1.Location = New System.Drawing.Point(20, 27)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(66, 17)
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
         Me.Label1.TabIndex = 7
         Me.Label1.Text = "Identidad"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(27, 137)
+        Me.Label3.Location = New System.Drawing.Point(20, 111)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(119, 17)
+        Me.Label3.Size = New System.Drawing.Size(90, 13)
         Me.Label3.TabIndex = 9
         Me.Label3.Text = "Segundo Nombre"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(27, 82)
+        Me.Label2.Location = New System.Drawing.Point(20, 67)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(103, 17)
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
         Me.Label2.TabIndex = 8
         Me.Label2.Text = "Primer Nombre"
         '
-        'cmbMunicipio
+        'ErrorValidacion
         '
-        Me.cmbMunicipio.FormattingEnabled = True
-        Me.cmbMunicipio.Items.AddRange(New Object() {"Comayagua", "Ajuterique", "El Rosario", "Esquías", "Humuya", "La Libertad", "Lamaní", "La Trinidad", "Lejamaní", "Meámbar", "Minas de Oro", "Ojos de Agua", "San Jerónimo", "San José de Comayagua", "San José del Potrero", "San Luis", "San Sebastián", "Siguatepeque", "Villa de San Antonio", "Las Lajas", "Taulabé"})
-        Me.cmbMunicipio.Location = New System.Drawing.Point(455, 183)
-        Me.cmbMunicipio.Name = "cmbMunicipio"
-        Me.cmbMunicipio.Size = New System.Drawing.Size(121, 24)
-        Me.cmbMunicipio.TabIndex = 17
-        '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(329, 190)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(67, 17)
-        Me.Label9.TabIndex = 16
-        Me.Label9.Text = "Municipio"
+        Me.ErrorValidacion.ContainerControl = Me
         '
         'Crud_Alcaldes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Crud_Alcaldes"
         Me.Text = "Crud_Alcaldes"
         Me.GroupBox3.ResumeLayout(False)
@@ -291,6 +326,7 @@ Partial Class Crud_Alcaldes
         Me.GroupBox2.PerformLayout()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.ErrorValidacion, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -318,4 +354,6 @@ Partial Class Crud_Alcaldes
     Friend WithEvents Label2 As Label
     Friend WithEvents cmbMunicipio As ComboBox
     Friend WithEvents Label9 As Label
+    Friend WithEvents ErrorValidacion As ErrorProvider
+    Friend WithEvents ToolTip As ToolTip
 End Class
